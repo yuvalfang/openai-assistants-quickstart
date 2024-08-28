6fe2e71f2478e638743fa390584d5cf5c8c05a8f
yuvalfang <yuval.fang@gmail.com>
2024-08-28 14:52:49 -0700

    Added assistant ID

--- app/assistant-config.ts
+++ app/assistant-config.ts
@@ -1,4 +1,4 @@
-export let assistantId = ""; // set your assistant ID here
+export let assistantId = "asst_J66KAtjsEMJGPIV3lmpKF4iK"; // set your assistant ID here
 
 if (assistantId === "") {
   assistantId = process.env.OPENAI_ASSISTANT_ID;
